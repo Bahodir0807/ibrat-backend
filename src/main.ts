287fb3751ce7d347b21b631f7c1039a5349f3528
0bservero <panda@sultonoway.uz>
2026-01-25 22:21:49 +0500

    test

--- src/main.ts
+++ src/main.ts
@@ -15,23 +15,24 @@ async function bootstrap() {
   app.useGlobalInterceptors(new LoggingInterceptor());
   app.useGlobalPipes(new CustomValidationPipe());
   app.enableCors({
-    origin: (origin, callback) => {
-      const allowedOrigins = ['https://sultonoway.uz', 'http://localhost:5173', 'https://b.sultonoway.uz', 'http://localhost:3000'];
-      if (!origin || allowedOrigins.includes(origin)) callback(null, true);
-      else callback(new Error('Not allowed by CORS'));
-    },
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: [
-      'Content-Type',
-      'Authorization',
-      'Accept',
-      'Origin',
-      'X-Requested-With',
-      'Access-Control-Allow-Headers',
-    ],
-    exposedHeaders: ['Access-Control-Allow-Origin'],
-  });
+  origin: [
+    'https://sultonoway.uz',
+    'http://localhost:5173',
+    'https://b.sultonoway.uz',
+    'http://localhost:3000',
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: [
+    'Content-Type',
+    'Authorization',
+    'Accept',
+    'Origin',
+    'X-Requested-With',
+  ],
+  exposedHeaders: ['Access-Control-Allow-Origin'],
+});
+
 
   const telegramService = app.get(TelegramService);
   const bot = telegramService.getBot();
